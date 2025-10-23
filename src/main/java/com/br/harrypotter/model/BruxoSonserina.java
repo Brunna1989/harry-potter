2cef276b90cb117770165af25b3e1c4a995689c2
brunna.dornelles <brunnadornelles336@gmail.com>
2025-10-23 16:36:55 -0300

    Ajustes

--- src/main/java/com/br/harrypotter/model/BruxoSonserina.java
+++ src/main/java/com/br/harrypotter/model/BruxoSonserina.java
@@ -1,22 +1,4 @@
 package com.br.harrypotter.model;
 
-import jakarta.persistence.DiscriminatorValue;
-import jakarta.persistence.Entity;
-
-@Entity
-@DiscriminatorValue("Sonserina")
-public class BruxoSonserina extends Bruxo implements Magia {
-
-    public BruxoSonserina() {
-        super();
-    }
-
-    public BruxoSonserina(String nome) {
-        super(null, nome, "Sonserina");
-    }
-
-    @Override
-    public String lancarFeitico() {
-        return "Serpensortia! — O bruxo da Sonserina lançou seu feitiço!";
-    }
+public class BruxoSonserina {
 }
